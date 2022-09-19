0c61a88760755d7d7d39620de20af72fbde4a604
erick-romero <53836321+erick-romero@users.noreply.github.com>
2022-09-19 15:48:06 -0500

    Character Animations and Basic collision

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,17 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 100
-const ACCELERATION = 10
-const FRICTION = 10
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("Hello Game")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
+	
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -18,12 +19,15 @@ func _physics_process(delta):
 	inputVector = inputVector.normalized()
 	
 	if inputVector != Vector2.ZERO:
-		velocity += inputVector  * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		animationTree.set("parameters/Idle/blend_position",inputVector)
+		animationTree.set("parameters/run/blend_position",inputVector)
+		animationState.travel("run")
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity) 
+	velocity = move_and_slide(velocity) 
 
 #func _process(delta):
 #	pass
